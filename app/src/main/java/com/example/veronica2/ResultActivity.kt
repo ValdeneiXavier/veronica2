ea99a6b2efc8513bebbf994e27531150b4627b62
ValdeneiXavier <valdeneixavier@gmail.com>
2023-05-13 23:15:52 -0400

    ResultActivity criada

---
+++ app/src/main/java/com/example/veronica2/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.veronica2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
