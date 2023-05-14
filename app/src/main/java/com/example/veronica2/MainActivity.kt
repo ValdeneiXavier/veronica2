79b1f578bf49ffcd410dcc101ad2931a18842d16
ValdeneiXavier <valdeneixavier@gmail.com>
2023-05-14 00:04:26 -0400

    removendo bug da activity main e referenciando string do botão send_to_result

--- app/src/main/java/com/example/veronica2/MainActivity.kt
+++ app/src/main/java/com/example/veronica2/MainActivity.kt
@@ -1,26 +1,43 @@
 package com.example.veronica2
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-            val tvResultado = findViewById<TextView>(R.id.tvResultado)
-            val etNome = findViewById<EditText>(R.id.etNome)
-            val btEnviar = findViewById<Button>(R.id.btEnviar)
-            btEnviar.setOnClickListener {
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        val etNome = findViewById<EditText>(R.id.etNome)
+        val btEnviar = findViewById<Button>(R.id.btEnviar)
+        val btEnviar2 = findViewById<Button>(R.id.btEnviar2)
+
+        btEnviar.setOnClickListener {
             if (etNome.text.isNotBlank()) {
                 tvResultado.text = getString(R.string.hello_name, etNome.text.toString())
-            }else{
+            } else {
                 etNome.error = getString(R.string.type_your_name)
             }
+
+            btEnviar2.setOnClickListener {
+                if (etNome.text.isNotBlank()) {
+                    val nomeDigitado = etNome.text.toString()
+
+                    val intent = Intent(this, ResultActivity::class.java)
+
+                    intent.putExtra("NOME_DIGITADO", nomeDigitado)
+
+                    startActivity(intent)
+                } else {
+                    etNome.error = getString(R.string.type_your_name)
+                }
             }
 
+    }
     }
 }
--- app/src/main/java/com/example/veronica2/ResultActivity.kt
+++ app/src/main/java/com/example/veronica2/ResultActivity.kt
@@ -1,11 +1,16 @@
 package com.example.veronica2
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val nomeDigitado = intent.getStringArrayExtra("NOME_DIGITADO")
+        val tvResultado = findViewById<TextView>(R.id.tvResultado)
+        tvResultado.text = nomeDigitado.toString()
     }
 }
